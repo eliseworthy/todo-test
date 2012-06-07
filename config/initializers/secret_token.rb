65f7b2bf21bfb803e6aa43162debd8e113e28a49
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-07 10:45:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '1a90457d1e1ce76f2239bc06cd0978f2c13c6b787c0d053e3bb11c5195b0c0d7de95aa663628987abbe8afd844d6f05a41b87e1bcbd06f585ee26e720b2fa778'
